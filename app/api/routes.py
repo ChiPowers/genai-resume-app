c1e37fa2959e689a4bd5177e6cd6b2537fffdab6
Chivon Powers <chivonpowers@Chivons-MacBook-Pro.local>
2024-01-28 01:43:35 -0700

    add web interface

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,28 +1,36 @@
 # fullstack-resume/app/api/routes.py
 import os
 from . import api_blueprint
-from flask import request, jsonify
+from flask import request, jsonify, render_template
 from app.services import openai_service, chroma_service
 from app.utils.helper_functions import split_docs, build_prompt, load_docs
 
 
-
-@api_blueprint.route('/embed-and-store', methods=['POST'])
+@api_blueprint.route('/embed-and-store', methods=['GET'])
 def embed_and_store():
 	texts = load_docs(os.environ.get("rag_pdf_path"))
 	chunks = split_docs(texts)
 	chroma_service.embed_chunks_and_upload_to_chroma(chunks, os.environ.get("db_path"))
-	response_json = {
-		"message": "Document chunks embedded successfully"
-	}
-	return jsonify(response_json)
+	return "Document chunks embedded successfully"
 
 
 @api_blueprint.route('/handle-query', methods=['POST'])
-def handle_query():
+def handle_query() -> 'html':
 	# handles embedding the user's question
-	question = request.json['question']
+	question = request.form['the_question']
 	retriever = chroma_service.get_most_similar_chunks_for_query(os.environ.get("db_path"))
 	prompt = build_prompt()
 	answer = openai_service.get_llm_answer(prompt, retriever, question)
-	return jsonify({"question:": question, "answer:": answer})
+	print(answer)
+	return render_template('handle_query.html',
+						 the_title='Ask about My Work History',
+						 the_question=question,
+						 the_answer=answer,
+						 )
+
+
+@api_blueprint.route('/')
+@api_blueprint.route('/get-query', methods=['GET','POST'])
+def get_query():
+	return render_template('get_query.html',
+						the_title='Ask an Interview Question')
